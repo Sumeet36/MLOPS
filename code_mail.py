ee71262d9810e6162ea623107580aa55675c88ee
Sumeet36 <55999226+Sumeet36@users.noreply.github.com>
2020-05-25 04:54:30 +0530

    f1

---
+++ code_mail.py
@@ -0,0 +1,17 @@
+import smtplib
+import urllib.request as urllib
+# Senders email
+sender_email = "sumeetsumit8@gmail.com"
+# Receivers email
+rec_email = "sumeetsumit8@gmail.com"
+
+message = "Best Modelcreated..."
+# Initialize the server variable
+server = smtplib.SMTP('smtp.gmail.com', 587)
+# Start the server connection
+server.starttls()
+# Login
+server.login("sumeetsumit8@gmail", "SUMEETGAIROLA123")
+print("Login Success!")
+# Send Email
+server.sendmail("Sumeet Gairola", "sumeetsumit8@gmail", message)
